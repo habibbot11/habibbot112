56ed3f1d144c04fb4e7be8875570caba47e7e0da
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-10-23 07:23:39 +0600

    Create 00joy1.js

---
+++ scripts/cmds/00joy1.js
@@ -0,0 +1,22 @@
+ module.exports = {
+ config: {
+	 name: "00joy",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "জয়") {
+ return message.reply({
+ body: "     「জয় বস এখন কাজে বিজি আছেন যা বলার আমাকে বলতে পারেন🤗😚」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/BC9Qr14.jpeg")
+ });
+ }
+ }
+}
