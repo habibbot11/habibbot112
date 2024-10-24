4643a0dcd991e9fb9240053c8cda8fcdeaf954bf
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-10-24 08:48:16 +0600

    Create ☺️.js

---
+++ scripts/cmds/☺️.js
@@ -0,0 +1,22 @@
+ module.exports = {
+ config: {
+	 name: "☺️",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "☺️") {
+ return message.reply({
+ body: "     「︵❝།།🌻-\_________\n❥◎⃝ -𝐬𝐢𝐫༊!◎⃝\n༅༎আপনার༅༎নিখোঁজ༎ডায়েরীর༅\n⑅⃝»❥শেষ༅༎পৃষ্ঠায়✿࿐°\nღ﹏আমি༅༎ একাংশ༅༎কাব‍্য༅\n_____ღ᭄︵❝།།💚\n🫠🫠\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/5enmTAK.mp4")
+ });
+ }
+ }
+}
