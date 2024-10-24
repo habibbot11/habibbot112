8f6aaaecd0ffef83cab973ab6e49997523f1819b
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-10-24 08:04:04 +0600

    Update 😐.js

--- scripts/cmds/😐.js
+++ scripts/cmds/😐.js
@@ -1,24 +1,22 @@
-module.exports = {
-  config: {
-    name: "😐",
-    aliases: ["lm10"],
-    version: "1.0",
-    author: "Otineyyyy your dadddy👾😉",
-    countDown: 5,
-    role: 0,
-    shortDescription: "send you pic of messi",
-    longDescription: "",
-    category: "𝗝𝗢𝗬-𝗔𝗛𝗠𝗘𝗗",
-    guide: "{pn}"
-  },
+ module.exports = {
+ config: {
+	 name: "😐",
+	 version: "1.0",
+	 author: "AceGun",
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: "no prefix",
+ },
 
-  onStart: async function ({ message }) {
-   var link = [ "https://i.imgur.com/kGs0cHp.mp4",
-]
-
-let img = link[Math.floor(Math.random()*link.length)]
-message.send({
-  body: '「︵❛❛༎💚︵\n🌺🦋🦋🌺-যে তােমার রাগ অভিমান আলাদা করে বুঝবে😊\nসে তােমাকে দিনশেষে একবার হলেও খুজবে!🥰🥀🌹︵❛❛༎💚🌺\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬」',attachment: await global.utils.getStreamFromURL(img)
-})
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "😐") {
+ return message.reply({
+ body: " 「 ︵❛❛༎💚︵\n🌺🦋🦋🌺-যে তােমার রাগ অভিমান আলাদা করে বুঝবে😊\nসে তােমাকে দিনশেষে একবার হলেও খুজবে!🥰🥀🌹︵❛❛༎💚🌺\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/kGs0cHp.mp4")
+ });
+ }
+ }
 }
-     }
