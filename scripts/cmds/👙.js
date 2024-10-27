399007dd2ac6ed20f4c9c6785049de239ab1f47b
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-10-27 08:33:30 +0600

    Update 👙.js

--- scripts/cmds/👙.js
+++ scripts/cmds/👙.js
@@ -14,8 +14,8 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "👙") {
  return message.reply({
- body: " 「___😽🌹ღ۵࿐\n❥一༊ বিছানায় হিসু করে দেওয়ার পর🙊,\nমেয়ে টি😻Facebook a স্ট্যাটাস দিলো\nবিপদ কখনো বলে আসে না।🥀😌🐸\n༉💚🥰彡\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 」",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/v0HZw3L.mp4")
+ body: "     「𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 」",
+ attachment: await global.utils.getStreamFromURL("https://drive.google.com/uc?id=1xnxz-dKT90qxtVO1wKj9u0_m6gMg60i2")
  });
  }
  }
