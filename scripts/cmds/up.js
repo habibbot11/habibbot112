2a7f1c5ef8daeb0c1067915bd4e25fa05452b7c2
𝗠𝗗 𝗝𝗨𝗕𝗔𝗘𝗗 𝗔𝗛𝗠𝗘𝗗 𝗝𝗢𝗬 <134224832+MD-JUBAED-AHMED-JOY@users.noreply.github.com>
2024-11-24 22:33:28 +0600

    Create up.js

---
+++ scripts/cmds/up.js
@@ -0,0 +1,30 @@
+module.exports = {
+  config: {
+    name: "up",
+    aliases: ["up", "upt"],
+    version: "1.0",
+    author: "JOY-AHMED",
+    role: 0,
+    shortDescription: {
+      en: "Displays the uptime of the bot."
+    },
+    longDescription: {
+      en: "Displays the amount of time that the bot has been running for."
+    },
+    category: "System",
+    guide: {
+      en: "Use {p}uptime to display the uptime of the bot."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const uptime = process.uptime();
+    const seconds = Math.floor(uptime % 60);
+    const minutes = Math.floor((uptime / 60) % 60);
+    const hours = Math.floor((uptime / (60 * 60)) % 24);
+    const days = Math.floor(uptime / (60 * 60 * 24));
+    const uptimeString = `╭──𝐁𝐨𝐭 𝐔𝐩 𝐓𝐢𝐦𝐞~\n├🕰️ʜᴏᴜʀꜱ  ${hours}\n├⏰ᴍɪɴᴜᴛᴇꜱ ${minutes}\n├⏳ꜱᴇᴄᴏɴᴅꜱ ${seconds}\n╰──────────–————─✰`;
+    api.sendMessage(`${uptimeString}`, event.threadID);
+  }
+}
+
+
